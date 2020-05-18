1124b0d7b38c8be965fd1c3b2378f0551333fea4
ernestSanchez <ernest.art28@gmail.com>
2020-05-18 14:41:50 +0200

    arreglos y matrices

---
+++ Proyects/ejercicioJaget/ejercicioJaget/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioJaget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int salones = 0;
+            int alumnos = 0;
+
+            int i; //ciclo exterior (SALONES)
+            int j; //ciclo interior (alumnos)
+
+            //pedir la cantidad de salones 
+            Console.Write("Dame la cantidad de salones: ");
+            salones = Convert.ToInt32(Console.ReadLine());
+
+            //crear arreglo jaget 
+            double[][] calificaciones = new double[salones][];
+
+            Console.WriteLine();//espacio de linea 
+
+            //pedir alumnos por salon
+
+            for (i = 0; i < salones; i++)
+            {
+                Console.WriteLine("salon {0} ", i + 1);
+                Console.Write("Dame la cantidad de alumnos: ");
+                alumnos = Convert.ToInt32(Console.ReadLine());
+
+                //asignar los alumnos (instanciar el arreglo)
+                calificaciones[i] = new double[alumnos];               
+            }
+            Console.WriteLine();
+
+            //pedir calificaciones
+            for (i = 0; i < salones;  i++)
+            {
+                Console.WriteLine("salon {0} ", i + 1);
+                
+                for (j = 0; j < calificaciones[i].GetLength(0); j++)
+                {
+                    Console.Write("dame la calificacion del alumno {0}: ", j+1);
+                    calificaciones[i][j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            //mostrar info 
+            Console.WriteLine("\n\ninformacion\n");
+
+            for (i = 0; i < salones; i++)
+            {
+                Console.WriteLine("salon {0} ", i + 1);
+
+                for (j = 0; j < calificaciones[i].GetLength(0); j++)
+                {
+                    Console.WriteLine("el alumno {0},tiene {1} de calificacion", j + 1,calificaciones[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ Proyects/jaget/jaget/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jaget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 3;
+
+
+            //tipo[][] Nombre = new tipo[tamaño arreglo contenedor][]; <----sintaxis
+
+            //ejemplo 1
+            //////arreglo contenedor 
+            
+            ///////double[][] Prueba = new double[5][];
+
+            ///////inicializacion los arreglos:
+
+            /////////Prueba[0] = new double[a];
+            ////////////Prueba[1] = new double[5];
+            ////////////Prueba[2] = new double[1];
+            ////////////Prueba[3] = new double[4];
+            /////////////Prueba[4] = new double[2];
+
+
+            //ejemplo 2
+            /////forma ya sabiendo el numero e elementos ///
+
+            ////////double[][] calificaciones = new double[3][];
+            ////////calificaciones[0] = new double[] { 7.0, 10, 6.5 };
+
+            //ejemplo 3
+            //creamos los elementos con bloque de codigo 
+            //double[][] calificaciones = new double[][]
+            //{
+            //    new double[] { 7.0, 8.4},
+            //    new double[] { 7.0, 6.5,8.8}
+            //};
+
+        }
+    }
+}

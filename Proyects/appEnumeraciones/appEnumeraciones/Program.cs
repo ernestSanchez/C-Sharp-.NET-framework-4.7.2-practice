b35f8989e60bf5d164d3d78b3b1da55155b1fb14
ernestSanchez <ernest.art28@gmail.com>
2020-05-21 13:07:45 +0200

    enumeraciones

---
+++ Proyects/appEnumeraciones/appEnumeraciones/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appEnumeraciones
+{
+    class Program
+    {
+        enum mes {Enero,Febrero,Marzo,Abril,Mayo,Junio,Julio,Agosto,Septiembre,Octubre,Noviembre,Diciembre};
+
+        enum color {Rojo = 4, Negro, Purpura};
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            //declarar variable tipo mes 
+            mes miMes;
+
+            //asignamos valor  
+            miMes = mes.Mayo;
+
+            //Pasamos de enumeracio a entero 
+            numero = (int)miMes;
+
+            //mostramos la info 
+            Console.WriteLine("El mes es: {0}, con valor de: {1}", miMes, numero);
+
+            //crear variable tipo color 
+            color miColor = color.Negro;
+
+            //pasamos el valor a un numero 
+            numero = (int)miColor;
+
+            //mostramos la info 
+            Console.WriteLine("El color es: {0}, con valor de: {1}", miColor, numero);
+
+        }
+    }
+}
---
+++ Proyects/enumeraciones/enumeraciones/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumeraciones
+{
+    class Program
+    {
+        //declarar enumeracion 
+        //enum nombre {valor1,valor2, ...valor}; <--- sintax
+        enum semana {Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo};
+
+    //enum semana { Lunes = 5, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo };//<-- para iniciar la numeracion como quiera 
+
+        static void Main(string[] args)
+        {
+            //declaramos la variable
+            semana elDia;
+
+            //le asignamos valor 
+            elDia = semana.Sabado;
+
+            //metodo toString()
+            String mensaje = "El dia es ";
+            mensaje = mensaje + elDia.ToString();
+            Console.WriteLine(mensaje);
+
+            //metodo tipo dato 
+            Console.WriteLine("eL dia es: {0}", elDia);
+
+            //metodo numerico asignar numero 
+            int valor = 0;
+            valor = (int)elDia;
+            Console.WriteLine(valor);
+
+            //experesion arigmetica 
+            int salario = 0;
+            int pagoDia = 200;
+
+            salario = pagoDia * ((int)elDia);
+
+            if(elDia == semana.Viernes)
+            {
+                .....
+            }
+
+        }
+    }
+}
